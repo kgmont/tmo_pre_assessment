ed05e4d579e57edde4fb8f3bd4311dcde1ae1355
KMontil3 <Kenny.Montilus3@T-Mobile.com>
2022-01-12 22:35:02 -0500

    updated flask runner

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, jsonify, request
 import sqlite3
 from flask_cors import CORS
+import os
 
 app = Flask(__name__)
 CORS(app)
@@ -66,4 +67,5 @@ def deleteAllBooks():
 
 
 if __name__ == "__main__":
-    app.run()
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
